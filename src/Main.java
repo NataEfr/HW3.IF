e6aab798babac2dc5e4f92e59161179866d8eacf
Nataliia <nataliefremova88@yandex.ru>
2026-01-18 17:09:55 +0300

    T1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //1
 
+        int age = 21;
+        if (age<=17) {
+            System.out.println("Если возраст человека равен "+age+", то  он не достиг совершеннолетия, нужно немного подождать.");
+        } else {
+            System.out.println("Если возраст человека равен"+age+", то  он соврешеннолетний.");
+        }
         }
     }
