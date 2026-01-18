9c2e458eda9f444009cd049a4c5d3c4e521a6ac8
Nataliia <nataliefremova88@yandex.ru>
2026-01-18 18:18:42 +0300

    T4

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,48 @@ public class Main {
         //1
 
         int age = 21;
-        if (age<=17) {
-            System.out.println("Если возраст человека равен "+age+", то  он не достиг совершеннолетия, нужно немного подождать.");
+        if (age <= 17) {
+            System.out.println("Если возраст человека равен " + age + ", то  он не достиг совершеннолетия, нужно немного подождать.");
         } else {
-            System.out.println("Если возраст человека равен"+age+", то  он соврешеннолетний.");
+            System.out.println("Если возраст человека равен " + age + ", то  он соврешеннолетний.");
         }
 
         //2
 
         double temp = 25.8d;
-        if (temp<=5){
-            System.out.println("На улице "+temp+" градусов, нужно надеть шапку");
+        if (temp <= 5) {
+            System.out.println("На улице " + temp + " градусов, нужно надеть шапку");
         } else {
 
-            System.out.println("На улице "+temp+" градусов, можно идти без шапки");
+            System.out.println("На улице " + temp + " градусов, можно идти без шапки");
         }
+
+        //3
+        int fast = 120;
+        if (fast <= 60) {
+            System.out.println("«Если скорость " + fast + " км/ч,то можно ездить спокойно».");
+        } else {
+            System.out.println("«Если скорость " + fast + " км/ч,то придется заплатить штраф.");
+        }
+
+        //4
+        int peopleAge = 22;
+        boolean peopleAgeIsNotSeven = peopleAge >= 2 && peopleAge <= 6;
+        boolean peopleAgeIsMoreSix = peopleAge > 6 && peopleAge <= 17;
+        boolean peopleAgeIsMore18 = peopleAge >= 18 && peopleAge <= 24;
+        if (peopleAgeIsNotSeven) {
+            System.out.println("Если возраст человека равен " + peopleAge + ", то ему нужно ходить в детский сад");
+        }
+        else if (peopleAgeIsMoreSix) {
+            System.out.println("Если возраст человека равен " + peopleAge + ", то ему нужно ходить в школу");
+        }
+         else if (peopleAgeIsMore18) {
+            System.out.println("Если возраст человека равен " + peopleAge + ", то ему нужно ходить в университет");
+        } else {
+            System.out.println("Если возраст человека равен " + peopleAge + ", то ему нужно ходить на работу");
+        }
+
+
     }
-}
+    }
+
